f09334fafd0a90ab0113fe2f0dfb074913076547
ItsTraderbaby <itstraderbaby@gmail.com>
2025-08-16 20:58:00 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BooksOS from '@/books/BooksOS'
+
+export default function App(){
+  return (
+    <main className="min-h-screen">
+      <BooksOS/>
+    </main>
+  )
+}
